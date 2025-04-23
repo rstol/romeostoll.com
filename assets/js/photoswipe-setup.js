6f8c140f895b15bdfdc7ae102f331f9e13abb431
rstol <47355990+rstol@users.noreply.github.com>
2025-04-23 08:25:21 +0000

    Deploying to gh-pages from @ rstol/romeostoll.com@417e628f6ff7ba5c43dbe9ee021642d8d9f37144 🚀

---
+++ assets/js/photoswipe-setup.js
@@ -0,0 +1,11 @@
+import PhotoSwipeLightbox from "https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe-lightbox.esm.min.js";
+import PhotoSwipe from "https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe.esm.min.js";
+const photoswipe = new PhotoSwipeLightbox({
+  gallery: ".pswp-gallery",
+  children: "a",
+  pswpModule: PhotoSwipe,
+  initialZoomLevel: 'fit',
+  secondaryZoomLevel: 1,
+  maxZoomLevel: 2,
+});
+photoswipe.init();
---
+++ assets/js/search-data.js
@@ -0,0 +1,106 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-home",
+    title: "Home",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-articles",
+          title: "Articles",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-github",
+          title: "Github",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "nav-gallery",
+          title: "Gallery",
+          description: "Travel pictures and videos",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/gallery/";
+          },
+        },{id: "nav-book-summaries-and-reviews",
+          title: "Book Summaries and Reviews",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/books/";
+          },
+        },{id: "books-briefe-an-einen-jungen-dichter",
+          title: 'Briefe an einen jungen Dichter',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/briefe_an_einen_jungen_dichter/";
+            },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%72%6F%6D%65%6F@%72%6F%6D%65%6F%73%74%6F%6C%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/romeo-stoll-276238171", "_blank");
+        },
+      },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
