f7d4e6fb59ed49321d5936b29435d5ae8fc32d6d
rstol <47355990+rstol@users.noreply.github.com>
2025-07-07 17:07:07 +0000

    Deploying to gh-pages from @ rstol/romeostoll.com@47e0fe5219f593e7ed6355cc97c99b29e950b54e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "A growing collection of cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
